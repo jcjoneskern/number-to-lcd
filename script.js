fd543a8b6d37ae0dd82920ebea224e80582965eb
J-C <jcjoneskern@gmail.com>
2017-04-28 14:06:42 -0400

    dry-er

--- script.js
+++ script.js
@@ -6,28 +6,16 @@ function converter(num) {
   if(num < 10) {
     return outputs[num];
   } else {
-    //split the number into individual digits
-    //pull info from the array based on those digits
-    //cut and splice the numbers together
-    //ie 1,2,3,1,2,3\n6,7,8,6,7,8\n11,12,13,11,12,13 for two digit numbers
-    //return the spliced numbers
-
     num = num.toString();
     num = num.split('').map(Number);
 
-    for(let i = 0; i < num.length; i++) {
-      output += outputs[num[i]].substr(0,3);
+    for(let i = 0; i < 9; i += 4) {
+      for(let j = 0; j < num.length; j++) {
+        output += outputs[num[j]].substr(i,3);
+      }
+      output += "\n";
     }
-    output += "\n";
-
-    for(let i = 0; i < num.length; i++) {
-      output += outputs[num[i]].substr(4,3);
-    }
-    output += "\n";
-
-    for(let i = 0; i < num.length; i++) {
-      output += outputs[num[i]].substr(8,3);
-    }
+    output = output.substr(0, output.length - 1);
 
     return output;
   }
